287df773c54bb20a947e7e8e1a801814d77581e7
Leandro <leandroscarpigranelli@gmail.com>
2023-04-04 11:20:57 -0300

    Primeiro commit

---
+++ src/aula2/TCPServer.java
@@ -0,0 +1,44 @@
+package aula2;//
+//  TCPServer.java
+//
+//  Kurose & Ross
+//
+import java.io.*;
+import java.net.*;
+
+class TCPServer {
+
+    public static void main (String args[]) throws Exception {
+		// throws Exception here because don't want to deal
+		// with errors in the rest of the code for simplicity.
+		// (Note: NOT a good practice).
+		
+         //Welcome socket  ---- SOCKET 1
+		 ServerSocket serverSocket = new ServerSocket(9000);
+		 // waits for a new connection. Accepts connection from multiple clients
+		 while (true) {
+			 System.out.println("Waiting for connection at port 9000.");
+             //Connection socket  --- SOCKET 2
+			 Socket s = serverSocket.accept();
+			 System.out.println("Connection established from " + s.getInetAddress());
+			 
+			 // create a BufferedReader object to read strings from
+			 // the socket. (read strings FROM CLIENT)
+			 BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			 String input = br.readLine();
+			 
+			 //create output stream to write to/send TO CLINET
+             DataOutputStream output = new DataOutputStream(s.getOutputStream());
+			 
+			 // keep repeating until an empty line is read.
+			 while (input.compareTo("") != 0) {
+				 // convert input to upper case and echo back to
+				 // client.
+				 output.writeBytes(input.toUpperCase() + "\n");
+				 input = br.readLine();
+			}
+			// close current connection
+			s.close();
+		 }
+    }
+}
